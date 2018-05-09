1b242a021da96622b05cc94f54f0cdd2df518106
BryanYu <ak78912@hotmail.com>
2018-05-09 22:47:30 +0800

    OAuthSample Init

---
+++ OAuthSample/OAuthSample.AuthServer/App_Start/Startup.Auth.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Infrastructure;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+
+namespace OAuthSample.AuthServer
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseOAuthAuthorizationServer(
+                new OAuthAuthorizationServerOptions()
+                {
+                    AuthorizeEndpointPath = new PathString("/Authorize"),
+                    TokenEndpointPath = new PathString("/Token"),
+                    ApplicationCanDisplayErrors = true,
+                    AllowInsecureHttp = true,
+                    Provider = new OAuthAuthorizationServerProvider()
+                    {
+                        OnValidateClientRedirectUri = OnValidateClientRedirectUri,
+                        OnValidateClientAuthentication = OnValidateClientAuthentication,
+                        OnGrantResourceOwnerCredentials = OnGrantResourceOwnerCredentials,
+                        OnGrantClientCredentials = OnGrantClientCredentials,
+                    },
+                    AuthorizationCodeProvider = new AuthenticationTokenProvider
+                    {
+                        OnCreate = CreateAuthenticationCode,
+                        OnReceive = ReceiveAuthenticationCode
+                    },
+                    RefreshTokenProvider = new AuthenticationTokenProvider
+                    {
+                        OnCreate = CreateRefreshToken,
+                        OnReceive = ReceiveRefreshToken
+                    }
+                });
+        }
+
+        public Task OnValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            return null;
+        }
+
+        public Task OnValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            return null;
+        }
+
+        public Task OnGrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            return null;
+        }
+
+        public Task OnGrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            return null;
+        }
+
+        public void CreateAuthenticationCode(AuthenticationTokenCreateContext context)
+        {
+        }
+
+        public void ReceiveAuthenticationCode(AuthenticationTokenReceiveContext context)
+        {
+        }
+
+        public void CreateRefreshToken(AuthenticationTokenCreateContext context)
+        {
+        }
+
+        public void ReceiveRefreshToken(AuthenticationTokenReceiveContext context)
+        {
+        }
+    }
+}
---
+++ OAuthSample/OAuthSample.AuthServer/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OAuthSample.AuthServer.Startup))]
+
+namespace OAuthSample.AuthServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
